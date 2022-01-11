5d710e20c29e435de9c970310cc0882e64591fa5
rhdrnwndsl <tlswlsdn2457@gmail.com>
2022-01-12 00:51:49 +0900

    번호생성기 추가(모든번호나온수만큼 + 100회차 나온수만큼 + 10회차 나온수만큼 더해서 번호값생성)

--- app/src/main/java/com/jiw/powerlotto/Database.java
+++ app/src/main/java/com/jiw/powerlotto/Database.java
@@ -25,6 +25,9 @@ public class Database {
     public static final String DB_QRCHECK_TABLENAME = "TABLE_QRCHECK";
     public static final String DB_ALLCOMPILENUMBER_TABLENAME = "TABLE_ALLCOMPILENUMBER";
 
+    public static final String DB_100COMPILENUMBER_TABLENAME = "TABLE_100COMPILENUMBER";
+    public static final String DB_10COMPILENUMBER_TABLENAME = "TABLE_10COMPILENUMBER";
+
     /**
      * DB 에는 QR당첨확인한 것과 모든당첨회차번호를 저장한다
      */
@@ -47,6 +50,16 @@ public class Database {
                     "bonus VARCHAR(2)); ";
             mSqliteDB.execSQL(Create);
 
+            Create = "CREATE TABLE IF NOT EXISTS " + DB_100COMPILENUMBER_TABLENAME + "(ID INTEGER primary key autoincrement," +
+                    "drwNo INTEGER,no1 VARCHAR(2),no2 VARCHAR(2),no3 VARCHAR(2),no4 VARCHAR(2),no5 VARCHAR(2),no6 VARCHAR(2)," +
+                    "bonus VARCHAR(2)); ";
+            mSqliteDB.execSQL(Create);
+
+            Create = "CREATE TABLE IF NOT EXISTS " + DB_10COMPILENUMBER_TABLENAME + "(ID INTEGER primary key autoincrement," +
+                    "drwNo INTEGER,no1 VARCHAR(2),no2 VARCHAR(2),no3 VARCHAR(2),no4 VARCHAR(2),no5 VARCHAR(2),no6 VARCHAR(2)," +
+                    "bonus VARCHAR(2)); ";
+            mSqliteDB.execSQL(Create);
+
 
         }
         catch (SQLiteException ex)
@@ -58,22 +71,80 @@ public class Database {
 
     public void InsertQRCheckData(String _url, String _result)
     {
-        String insert = "INSERT INTO " + DB_QRCHECK_TABLENAME +
-                " (url,result)" +
-                " values ('"+ _url + "','" + _result +"');";
-        mSqliteDB.execSQL(insert);
+        try {
+            String insert = "INSERT INTO " + DB_QRCHECK_TABLENAME +
+                    " (url,result)" +
+                    " values ('" + _url + "','" + _result + "');";
+            mSqliteDB.execSQL(insert);
+        }
+        catch (SQLiteException ex)
+        {
+            Log.d(TAG,ex.toString());
+        }
     }
 
     public void InsertPreviewData(int _drwNo, String _no1, String _no2, String _no3,
                                   String _no4, String _no5, String _no6, String _bonus)
     {
-        String insert = "INSERT INTO " + DB_ALLCOMPILENUMBER_TABLENAME +
-                " (drwNo,no1,no2,no3,no4,no5,no6,bonus)" +
-                " values ('"+ _drwNo + "','" + _no1 + "','" + _no2 + "','" + _no3 + "'," +
-                "'" + _no4 + "','" + _no5 + "','" + _no6 + "','" + _bonus +"');";
-        mSqliteDB.execSQL(insert);
+        try {
+            String insert = "INSERT INTO " + DB_ALLCOMPILENUMBER_TABLENAME +
+                    " (drwNo,no1,no2,no3,no4,no5,no6,bonus)" +
+                    " values ('" + _drwNo + "','" + _no1 + "','" + _no2 + "','" + _no3 + "'," +
+                    "'" + _no4 + "','" + _no5 + "','" + _no6 + "','" + _bonus + "');";
+            mSqliteDB.execSQL(insert);
 
-        InsertNumberSortingData(_no1,_no2,_no3,_no4,_no5,_no6,_bonus);
+            InsertNumberSortingData(_no1, _no2, _no3, _no4, _no5, _no6, _bonus);
+
+            InsertPreview100Data(_drwNo,_no1,_no2,_no3,_no4,_no5,_no6,_bonus);
+        }
+        catch (SQLiteException ex)
+        {
+            Log.d(TAG,ex.toString());
+        }
+    }
+
+    public void InsertPreview100Data(int _drwNo, String _no1, String _no2, String _no3,
+                                  String _no4, String _no5, String _no6, String _bonus)
+    {
+        try
+        {
+            DeleteCheckCount(DB_100COMPILENUMBER_TABLENAME, 100);
+            String insert = "INSERT INTO " + DB_100COMPILENUMBER_TABLENAME +
+                    " (drwNo,no1,no2,no3,no4,no5,no6,bonus)" +
+                    " values ('"+ _drwNo + "','" + _no1 + "','" + _no2 + "','" + _no3 + "'," +
+                    "'" + _no4 + "','" + _no5 + "','" + _no6 + "','" + _bonus +"');";
+            mSqliteDB.execSQL(insert);
+
+            InsertNumber100SortingData(_no1,_no2,_no3,_no4,_no5,_no6,_bonus);
+
+            InsertPreview10Data(_drwNo,_no1,_no2,_no3,_no4,_no5,_no6,_bonus);
+        }
+        catch (SQLiteException ex)
+        {
+            Log.d(TAG,ex.toString());
+        }
+
+    }
+
+    public void InsertPreview10Data(int _drwNo, String _no1, String _no2, String _no3,
+                                     String _no4, String _no5, String _no6, String _bonus)
+    {
+        try
+        {
+            DeleteCheckCount(DB_10COMPILENUMBER_TABLENAME, 10);
+            String insert = "INSERT INTO " + DB_10COMPILENUMBER_TABLENAME +
+                    " (drwNo,no1,no2,no3,no4,no5,no6,bonus)" +
+                    " values ('"+ _drwNo + "','" + _no1 + "','" + _no2 + "','" + _no3 + "'," +
+                    "'" + _no4 + "','" + _no5 + "','" + _no6 + "','" + _bonus +"');";
+            mSqliteDB.execSQL(insert);
+
+            InsertNumber10SortingData(_no1,_no2,_no3,_no4,_no5,_no6,_bonus);
+        }
+        catch (SQLiteException ex)
+        {
+            Log.d(TAG,ex.toString());
+        }
+
     }
 
 
@@ -111,6 +182,78 @@ public class Database {
 
         Log.d(TAG,_no1 + " : " + (NO1+1) + ", " + _no2 + " : " + (NO2+1) + ", " + _no3 + " : " + (NO3+1) + ", " +
                 _no4 + " : " + (NO4+1) + ", " + _no5 + " : " + (NO5+1) + ", " + _no6 + " : " + (NO6+1) + ", " + _bonus + " : " + (BONUS+1));
+    }
+
+    public void InsertNumber100SortingData(String _no1, String _no2, String _no3, String _no4,
+                                        String _no5, String _no6, String _bonus)
+    {
+        int NO1 = 0;
+        int NO2 = 0;
+        int NO3 = 0;
+        int NO4 = 0;
+        int NO5 = 0;
+        int NO6 = 0;
+        int BONUS = 0;
+
+        NO1 = PowerSDK.getIntPreference(mCtx,"100_" + _no1);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no1, NO1 + 1);
+
+        NO2 = PowerSDK.getIntPreference(mCtx,"100_" + _no2);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no2, NO2 + 1);
+
+        NO3 = PowerSDK.getIntPreference(mCtx,"100_" + _no3);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no3, NO3 + 1);
+
+        NO4 = PowerSDK.getIntPreference(mCtx,"100_" + _no4);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no4, NO4 + 1);
+
+        NO5 = PowerSDK.getIntPreference(mCtx,"100_" + _no5);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no5, NO5 + 1);
+
+        NO6 = PowerSDK.getIntPreference(mCtx,"100_" + _no6);
+        PowerSDK.setIntPreference(mCtx, "100_" + _no6, NO6 + 1);
+
+        BONUS = PowerSDK.getIntPreference(mCtx,"100_" + _bonus);
+        PowerSDK.setIntPreference(mCtx, "100_" + _bonus, BONUS + 1);
+
+        Log.d(TAG,"100_" + _no1 + " : " + (NO1+1) + ", " + "100_" + _no2 + " : " + (NO2+1) + ", " + "100_" + _no3 + " : " + (NO3+1) + ", " +
+                "100_" + _no4 + " : " + (NO4+1) + ", " + "100_" + _no5 + " : " + (NO5+1) + ", " + "100_" + _no6 + " : " + (NO6+1) + ", " + "100_" + _bonus + " : " + (BONUS+1));
+    }
+
+    public void InsertNumber10SortingData(String _no1, String _no2, String _no3, String _no4,
+                                        String _no5, String _no6, String _bonus)
+    {
+        int NO1 = 0;
+        int NO2 = 0;
+        int NO3 = 0;
+        int NO4 = 0;
+        int NO5 = 0;
+        int NO6 = 0;
+        int BONUS = 0;
+
+        NO1 = PowerSDK.getIntPreference(mCtx,"10_" + _no1);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no1, NO1 + 1);
+
+        NO2 = PowerSDK.getIntPreference(mCtx,"10_" + _no2);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no2, NO2 + 1);
+
+        NO3 = PowerSDK.getIntPreference(mCtx,"10_" + _no3);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no3, NO3 + 1);
+
+        NO4 = PowerSDK.getIntPreference(mCtx,"10_" + _no4);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no4, NO4 + 1);
+
+        NO5 = PowerSDK.getIntPreference(mCtx,"10_" + _no5);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no5, NO5 + 1);
+
+        NO6 = PowerSDK.getIntPreference(mCtx,"10_" + _no6);
+        PowerSDK.setIntPreference(mCtx, "10_" + _no6, NO6 + 1);
+
+        BONUS = PowerSDK.getIntPreference(mCtx,"10_" + _bonus);
+        PowerSDK.setIntPreference(mCtx, "10_" + _bonus, BONUS + 1);
+
+        Log.d(TAG,"10_" + _no1 + " : " + (NO1+1) + ", " + "10_" + _no2 + " : " + (NO2+1) + ", " + "10_" + _no3 + " : " + (NO3+1) + ", " +
+                "10_" + _no4 + " : " + (NO4+1) + ", " + "10_" + _no5 + " : " + (NO5+1) + ", " + "10_" +  _no6 + " : " + (NO6+1) + ", " + "10_" + _bonus + " : " + (BONUS+1));
     }
 
     public String[] SelectData(String TableName)
@@ -164,6 +307,24 @@ public class Database {
         //기존 데이터 삭제
         String delete = "DELETE FROM " + DB_QRCHECK_TABLENAME + " where url = '" + _url  + "';";
         mSqliteDB.execSQL(delete);
+    }
+
+    private void DeleteCheckCount(String _tableName, int MaxCount)
+    {
+        int ColumnsCount = 0;
+        String selectQuery = "Select * from " + _tableName ;
+        Cursor c = mSqliteDB.rawQuery(selectQuery,null);
+
+        while (c.moveToNext())
+        {
+            ColumnsCount++;
+        }
+
+        if(ColumnsCount> (MaxCount-1))
+        {
+            String delete = "DELETE FROM " + _tableName + " where ID = (SELECT MIN(ID) FROM " + _tableName + ");";
+            mSqliteDB.execSQL(delete);
+        }
     }
 
 }
--- app/src/main/java/com/jiw/powerlotto/NumberGeneratorActivity.java
+++ app/src/main/java/com/jiw/powerlotto/NumberGeneratorActivity.java
@@ -49,27 +49,55 @@ public class NumberGeneratorActivity extends AppCompatActivity {
 
 
         Map<String, Integer> map = new HashMap<>();
-        map.put("1", mPowerSDK.getIntPreference(mCtx,"1"));map.put("11", mPowerSDK.getIntPreference(mCtx,"11"));map.put("21", mPowerSDK.getIntPreference(mCtx,"21"));
-        map.put("2", mPowerSDK.getIntPreference(mCtx,"2"));map.put("12", mPowerSDK.getIntPreference(mCtx,"12"));map.put("22", mPowerSDK.getIntPreference(mCtx,"22"));
-        map.put("3", mPowerSDK.getIntPreference(mCtx,"3"));map.put("13", mPowerSDK.getIntPreference(mCtx,"13"));map.put("23", mPowerSDK.getIntPreference(mCtx,"23"));
-        map.put("4", mPowerSDK.getIntPreference(mCtx,"4"));map.put("14", mPowerSDK.getIntPreference(mCtx,"14"));map.put("24", mPowerSDK.getIntPreference(mCtx,"24"));
-        map.put("5", mPowerSDK.getIntPreference(mCtx,"5"));map.put("15", mPowerSDK.getIntPreference(mCtx,"15"));map.put("25", mPowerSDK.getIntPreference(mCtx,"25"));
-        map.put("6", mPowerSDK.getIntPreference(mCtx,"6"));map.put("16", mPowerSDK.getIntPreference(mCtx,"16"));map.put("26", mPowerSDK.getIntPreference(mCtx,"26"));
-        map.put("7", mPowerSDK.getIntPreference(mCtx,"7"));map.put("17", mPowerSDK.getIntPreference(mCtx,"17"));map.put("27", mPowerSDK.getIntPreference(mCtx,"27"));
-        map.put("8", mPowerSDK.getIntPreference(mCtx,"8"));map.put("18", mPowerSDK.getIntPreference(mCtx,"18"));map.put("28", mPowerSDK.getIntPreference(mCtx,"28"));
-        map.put("9", mPowerSDK.getIntPreference(mCtx,"9"));map.put("19", mPowerSDK.getIntPreference(mCtx,"19"));map.put("29", mPowerSDK.getIntPreference(mCtx,"29"));
-        map.put("10", mPowerSDK.getIntPreference(mCtx,"10")); map.put("20", mPowerSDK.getIntPreference(mCtx,"20")); map.put("30", mPowerSDK.getIntPreference(mCtx,"30"));
+        map.put("1", mPowerSDK.getIntPreference(mCtx,"1") + mPowerSDK.getIntPreference(mCtx,"100_1") + mPowerSDK.getIntPreference(mCtx,"10_1"));
+        map.put("2", mPowerSDK.getIntPreference(mCtx,"2") + mPowerSDK.getIntPreference(mCtx,"100_2") + mPowerSDK.getIntPreference(mCtx,"10_2"));
+        map.put("3", mPowerSDK.getIntPreference(mCtx,"3") + mPowerSDK.getIntPreference(mCtx,"100_3") + mPowerSDK.getIntPreference(mCtx,"10_3"));
+        map.put("4", mPowerSDK.getIntPreference(mCtx,"4") + mPowerSDK.getIntPreference(mCtx,"100_4") + mPowerSDK.getIntPreference(mCtx,"10_4"));
+        map.put("5", mPowerSDK.getIntPreference(mCtx,"5") + mPowerSDK.getIntPreference(mCtx,"100_5") + mPowerSDK.getIntPreference(mCtx,"10_5"));
+        map.put("6", mPowerSDK.getIntPreference(mCtx,"6") + mPowerSDK.getIntPreference(mCtx,"100_6") + mPowerSDK.getIntPreference(mCtx,"10_6"));
+        map.put("7", mPowerSDK.getIntPreference(mCtx,"7") + mPowerSDK.getIntPreference(mCtx,"100_7") + mPowerSDK.getIntPreference(mCtx,"10_7"));
+        map.put("8", mPowerSDK.getIntPreference(mCtx,"8") + mPowerSDK.getIntPreference(mCtx,"100_8") + mPowerSDK.getIntPreference(mCtx,"10_8"));
+        map.put("9", mPowerSDK.getIntPreference(mCtx,"9") + mPowerSDK.getIntPreference(mCtx,"100_9") + mPowerSDK.getIntPreference(mCtx,"10_9"));
+        map.put("10", mPowerSDK.getIntPreference(mCtx,"10")+mPowerSDK.getIntPreference(mCtx,"100_10")+ mPowerSDK.getIntPreference(mCtx,"10_10"));
 
-        map.put("31", mPowerSDK.getIntPreference(mCtx,"31"));map.put("41", mPowerSDK.getIntPreference(mCtx,"41"));
-        map.put("32", mPowerSDK.getIntPreference(mCtx,"32"));map.put("42", mPowerSDK.getIntPreference(mCtx,"42"));
-        map.put("33", mPowerSDK.getIntPreference(mCtx,"33"));map.put("43", mPowerSDK.getIntPreference(mCtx,"43"));
-        map.put("34", mPowerSDK.getIntPreference(mCtx,"34"));map.put("44", mPowerSDK.getIntPreference(mCtx,"44"));
-        map.put("35", mPowerSDK.getIntPreference(mCtx,"35"));map.put("45", mPowerSDK.getIntPreference(mCtx,"45"));
-        map.put("36", mPowerSDK.getIntPreference(mCtx,"36"));
-        map.put("37", mPowerSDK.getIntPreference(mCtx,"37"));
-        map.put("38", mPowerSDK.getIntPreference(mCtx,"38"));
-        map.put("39", mPowerSDK.getIntPreference(mCtx,"39"));
-        map.put("40", mPowerSDK.getIntPreference(mCtx,"40"));
+        map.put("11", mPowerSDK.getIntPreference(mCtx,"11") + mPowerSDK.getIntPreference(mCtx,"100_11") + mPowerSDK.getIntPreference(mCtx,"10_11"));
+        map.put("12", mPowerSDK.getIntPreference(mCtx,"12") + mPowerSDK.getIntPreference(mCtx,"100_12") + mPowerSDK.getIntPreference(mCtx,"10_12"));
+        map.put("13", mPowerSDK.getIntPreference(mCtx,"13") + mPowerSDK.getIntPreference(mCtx,"100_13") + mPowerSDK.getIntPreference(mCtx,"10_13"));
+        map.put("14", mPowerSDK.getIntPreference(mCtx,"14") + mPowerSDK.getIntPreference(mCtx,"100_14") + mPowerSDK.getIntPreference(mCtx,"10_14"));
+        map.put("15", mPowerSDK.getIntPreference(mCtx,"15") + mPowerSDK.getIntPreference(mCtx,"100_15") + mPowerSDK.getIntPreference(mCtx,"10_15"));
+        map.put("16", mPowerSDK.getIntPreference(mCtx,"16") + mPowerSDK.getIntPreference(mCtx,"100_16") + mPowerSDK.getIntPreference(mCtx,"10_16"));
+        map.put("17", mPowerSDK.getIntPreference(mCtx,"17") + mPowerSDK.getIntPreference(mCtx,"100_17") + mPowerSDK.getIntPreference(mCtx,"10_17"));
+        map.put("18", mPowerSDK.getIntPreference(mCtx,"18") + mPowerSDK.getIntPreference(mCtx,"100_18") + mPowerSDK.getIntPreference(mCtx,"10_18"));
+        map.put("19", mPowerSDK.getIntPreference(mCtx,"19") + mPowerSDK.getIntPreference(mCtx,"100_19") + mPowerSDK.getIntPreference(mCtx,"10_19"));
+        map.put("20", mPowerSDK.getIntPreference(mCtx,"20") + mPowerSDK.getIntPreference(mCtx,"100_20") + mPowerSDK.getIntPreference(mCtx,"10_20"));
+
+        map.put("21", mPowerSDK.getIntPreference(mCtx,"21") + mPowerSDK.getIntPreference(mCtx,"100_21") + mPowerSDK.getIntPreference(mCtx,"10_21"));
+        map.put("22", mPowerSDK.getIntPreference(mCtx,"22") + mPowerSDK.getIntPreference(mCtx,"100_22") + mPowerSDK.getIntPreference(mCtx,"10_22"));
+        map.put("23", mPowerSDK.getIntPreference(mCtx,"23") + mPowerSDK.getIntPreference(mCtx,"100_23") + mPowerSDK.getIntPreference(mCtx,"10_23"));
+        map.put("24", mPowerSDK.getIntPreference(mCtx,"24") + mPowerSDK.getIntPreference(mCtx,"100_24") + mPowerSDK.getIntPreference(mCtx,"10_24"));
+        map.put("25", mPowerSDK.getIntPreference(mCtx,"25") + mPowerSDK.getIntPreference(mCtx,"100_25") + mPowerSDK.getIntPreference(mCtx,"10_25"));
+        map.put("26", mPowerSDK.getIntPreference(mCtx,"26") + mPowerSDK.getIntPreference(mCtx,"100_26") + mPowerSDK.getIntPreference(mCtx,"10_26"));
+        map.put("27", mPowerSDK.getIntPreference(mCtx,"27") + mPowerSDK.getIntPreference(mCtx,"100_27") + mPowerSDK.getIntPreference(mCtx,"10_27"));
+        map.put("28", mPowerSDK.getIntPreference(mCtx,"28") + mPowerSDK.getIntPreference(mCtx,"100_28") + mPowerSDK.getIntPreference(mCtx,"10_28"));
+        map.put("29", mPowerSDK.getIntPreference(mCtx,"29") + mPowerSDK.getIntPreference(mCtx,"100_29") + mPowerSDK.getIntPreference(mCtx,"10_29"));
+        map.put("30", mPowerSDK.getIntPreference(mCtx,"30") + mPowerSDK.getIntPreference(mCtx,"100_30") + mPowerSDK.getIntPreference(mCtx,"10_30"));
+
+        map.put("31", mPowerSDK.getIntPreference(mCtx,"31") + mPowerSDK.getIntPreference(mCtx,"100_31") + mPowerSDK.getIntPreference(mCtx,"10_31"));
+        map.put("32", mPowerSDK.getIntPreference(mCtx,"32") + mPowerSDK.getIntPreference(mCtx,"100_32") + mPowerSDK.getIntPreference(mCtx,"10_32"));
+        map.put("33", mPowerSDK.getIntPreference(mCtx,"33") + mPowerSDK.getIntPreference(mCtx,"100_33") + mPowerSDK.getIntPreference(mCtx,"10_33"));
+        map.put("34", mPowerSDK.getIntPreference(mCtx,"34") + mPowerSDK.getIntPreference(mCtx,"100_34") + mPowerSDK.getIntPreference(mCtx,"10_34"));
+        map.put("35", mPowerSDK.getIntPreference(mCtx,"35") + mPowerSDK.getIntPreference(mCtx,"100_35") + mPowerSDK.getIntPreference(mCtx,"10_35"));
+        map.put("36", mPowerSDK.getIntPreference(mCtx,"36") + mPowerSDK.getIntPreference(mCtx,"100_36") + mPowerSDK.getIntPreference(mCtx,"10_36"));
+        map.put("37", mPowerSDK.getIntPreference(mCtx,"37") + mPowerSDK.getIntPreference(mCtx,"100_37") + mPowerSDK.getIntPreference(mCtx,"10_37"));
+        map.put("38", mPowerSDK.getIntPreference(mCtx,"38") + mPowerSDK.getIntPreference(mCtx,"100_38") + mPowerSDK.getIntPreference(mCtx,"10_38"));
+        map.put("39", mPowerSDK.getIntPreference(mCtx,"39") + mPowerSDK.getIntPreference(mCtx,"100_39") + mPowerSDK.getIntPreference(mCtx,"10_39"));
+        map.put("40", mPowerSDK.getIntPreference(mCtx,"40") + mPowerSDK.getIntPreference(mCtx,"100_40") + mPowerSDK.getIntPreference(mCtx,"10_40"));
+
+        map.put("41", mPowerSDK.getIntPreference(mCtx,"41") + mPowerSDK.getIntPreference(mCtx,"100_41") + mPowerSDK.getIntPreference(mCtx,"10_41"));
+        map.put("42", mPowerSDK.getIntPreference(mCtx,"42") + mPowerSDK.getIntPreference(mCtx,"100_42") + mPowerSDK.getIntPreference(mCtx,"10_42"));
+        map.put("43", mPowerSDK.getIntPreference(mCtx,"43") + mPowerSDK.getIntPreference(mCtx,"100_43") + mPowerSDK.getIntPreference(mCtx,"10_43"));
+        map.put("44", mPowerSDK.getIntPreference(mCtx,"44") + mPowerSDK.getIntPreference(mCtx,"100_44") + mPowerSDK.getIntPreference(mCtx,"10_44"));
+        map.put("45", mPowerSDK.getIntPreference(mCtx,"45") + mPowerSDK.getIntPreference(mCtx,"100_45") + mPowerSDK.getIntPreference(mCtx,"10_45"));
 
 
         List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
@@ -112,22 +140,55 @@ public class NumberGeneratorActivity extends AppCompatActivity {
     {
         int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20,n21,n22,n23,n24,n25,
                 n26,n27,n28,n29,n30,n31,n32,n33,n34,n35,n36,n37,n38,n39,n40,n41,n42,n43,n44,n45;
-        n1 = mPowerSDK.getIntPreference(mCtx,"1");n11 = mPowerSDK.getIntPreference(mCtx,"11");n21 = mPowerSDK.getIntPreference(mCtx,"21");n31 = mPowerSDK.getIntPreference(mCtx,"31");
-        n2 = mPowerSDK.getIntPreference(mCtx,"2");n12 = mPowerSDK.getIntPreference(mCtx,"12");n22 = mPowerSDK.getIntPreference(mCtx,"22");n32 = mPowerSDK.getIntPreference(mCtx,"32");
-        n3 = mPowerSDK.getIntPreference(mCtx,"3");n13 = mPowerSDK.getIntPreference(mCtx,"13");n23 = mPowerSDK.getIntPreference(mCtx,"23");n33 = mPowerSDK.getIntPreference(mCtx,"33");
-        n4 = mPowerSDK.getIntPreference(mCtx,"4");n14 = mPowerSDK.getIntPreference(mCtx,"14");n24 = mPowerSDK.getIntPreference(mCtx,"24");n34 = mPowerSDK.getIntPreference(mCtx,"34");
-        n5 = mPowerSDK.getIntPreference(mCtx,"5");n15 = mPowerSDK.getIntPreference(mCtx,"15");n25 = mPowerSDK.getIntPreference(mCtx,"25");n35 = mPowerSDK.getIntPreference(mCtx,"35");
-        n6 = mPowerSDK.getIntPreference(mCtx,"6");n16 = mPowerSDK.getIntPreference(mCtx,"16");n26 = mPowerSDK.getIntPreference(mCtx,"26");n36 = mPowerSDK.getIntPreference(mCtx,"36");
-        n7 = mPowerSDK.getIntPreference(mCtx,"7");n17 = mPowerSDK.getIntPreference(mCtx,"17");n27 = mPowerSDK.getIntPreference(mCtx,"27");n37 = mPowerSDK.getIntPreference(mCtx,"37");
-        n8 = mPowerSDK.getIntPreference(mCtx,"8");n18 = mPowerSDK.getIntPreference(mCtx,"18");n28 = mPowerSDK.getIntPreference(mCtx,"28");n38 = mPowerSDK.getIntPreference(mCtx,"38");
-        n9 = mPowerSDK.getIntPreference(mCtx,"9");n19 = mPowerSDK.getIntPreference(mCtx,"19");n29 = mPowerSDK.getIntPreference(mCtx,"29");n39 = mPowerSDK.getIntPreference(mCtx,"39");
-        n10 = mPowerSDK.getIntPreference(mCtx,"10");n20 = mPowerSDK.getIntPreference(mCtx,"20");n30 = mPowerSDK.getIntPreference(mCtx,"30");n40 = mPowerSDK.getIntPreference(mCtx,"40");
+        n1 = mPowerSDK.getIntPreference(mCtx,"1") + mPowerSDK.getIntPreference(mCtx,"10_1") + mPowerSDK.getIntPreference(mCtx,"100_1");
+        n2 = mPowerSDK.getIntPreference(mCtx,"2") + mPowerSDK.getIntPreference(mCtx,"10_2") + mPowerSDK.getIntPreference(mCtx,"100_2");
+        n3 = mPowerSDK.getIntPreference(mCtx,"3") + mPowerSDK.getIntPreference(mCtx,"10_3") + mPowerSDK.getIntPreference(mCtx,"100_3");
+        n4 = mPowerSDK.getIntPreference(mCtx,"4") + mPowerSDK.getIntPreference(mCtx,"10_4") + mPowerSDK.getIntPreference(mCtx,"100_4");
+        n5 = mPowerSDK.getIntPreference(mCtx,"5") + mPowerSDK.getIntPreference(mCtx,"10_5") + mPowerSDK.getIntPreference(mCtx,"100_5");
+        n6 = mPowerSDK.getIntPreference(mCtx,"6") + mPowerSDK.getIntPreference(mCtx,"10_6") + mPowerSDK.getIntPreference(mCtx,"100_6");
+        n7 = mPowerSDK.getIntPreference(mCtx,"7") + mPowerSDK.getIntPreference(mCtx,"10_7") + mPowerSDK.getIntPreference(mCtx,"100_7");
+        n8 = mPowerSDK.getIntPreference(mCtx,"8") + mPowerSDK.getIntPreference(mCtx,"10_8") + mPowerSDK.getIntPreference(mCtx,"100_8");
+        n9 = mPowerSDK.getIntPreference(mCtx,"9") + mPowerSDK.getIntPreference(mCtx,"10_9") + mPowerSDK.getIntPreference(mCtx,"100_9");
+        n10 = mPowerSDK.getIntPreference(mCtx,"10") + mPowerSDK.getIntPreference(mCtx,"10_10") + mPowerSDK.getIntPreference(mCtx,"100_10");
 
-        n41 = mPowerSDK.getIntPreference(mCtx,"41");
-        n42 = mPowerSDK.getIntPreference(mCtx,"42");
-        n43 = mPowerSDK.getIntPreference(mCtx,"43");
-        n44 = mPowerSDK.getIntPreference(mCtx,"44");
-        n45 = mPowerSDK.getIntPreference(mCtx,"45");
+        n11 = mPowerSDK.getIntPreference(mCtx,"11") + mPowerSDK.getIntPreference(mCtx,"10_11") + mPowerSDK.getIntPreference(mCtx,"100_11");
+        n12 = mPowerSDK.getIntPreference(mCtx,"12") + mPowerSDK.getIntPreference(mCtx,"10_12") + mPowerSDK.getIntPreference(mCtx,"100_12");
+        n13 = mPowerSDK.getIntPreference(mCtx,"13") + mPowerSDK.getIntPreference(mCtx,"10_13") + mPowerSDK.getIntPreference(mCtx,"100_13");
+        n14 = mPowerSDK.getIntPreference(mCtx,"14") + mPowerSDK.getIntPreference(mCtx,"10_14") + mPowerSDK.getIntPreference(mCtx,"100_14");
+        n15 = mPowerSDK.getIntPreference(mCtx,"15") + mPowerSDK.getIntPreference(mCtx,"10_15") + mPowerSDK.getIntPreference(mCtx,"100_15");
+        n16 = mPowerSDK.getIntPreference(mCtx,"16") + mPowerSDK.getIntPreference(mCtx,"10_16") + mPowerSDK.getIntPreference(mCtx,"100_16");
+        n17 = mPowerSDK.getIntPreference(mCtx,"17") + mPowerSDK.getIntPreference(mCtx,"10_17") + mPowerSDK.getIntPreference(mCtx,"100_17");
+        n18 = mPowerSDK.getIntPreference(mCtx,"18") + mPowerSDK.getIntPreference(mCtx,"10_18") + mPowerSDK.getIntPreference(mCtx,"100_18");
+        n19 = mPowerSDK.getIntPreference(mCtx,"19") + mPowerSDK.getIntPreference(mCtx,"10_19") + mPowerSDK.getIntPreference(mCtx,"100_19");
+        n20 = mPowerSDK.getIntPreference(mCtx,"20") + mPowerSDK.getIntPreference(mCtx,"10_20") + mPowerSDK.getIntPreference(mCtx,"100_20");
+
+        n21 = mPowerSDK.getIntPreference(mCtx,"21") + mPowerSDK.getIntPreference(mCtx,"10_21") + mPowerSDK.getIntPreference(mCtx,"100_21");
+        n22 = mPowerSDK.getIntPreference(mCtx,"22") + mPowerSDK.getIntPreference(mCtx,"10_22") + mPowerSDK.getIntPreference(mCtx,"100_22");
+        n23 = mPowerSDK.getIntPreference(mCtx,"23") + mPowerSDK.getIntPreference(mCtx,"10_23") + mPowerSDK.getIntPreference(mCtx,"100_23");
+        n24 = mPowerSDK.getIntPreference(mCtx,"24") + mPowerSDK.getIntPreference(mCtx,"10_24") + mPowerSDK.getIntPreference(mCtx,"100_24");
+        n25 = mPowerSDK.getIntPreference(mCtx,"25") + mPowerSDK.getIntPreference(mCtx,"10_25") + mPowerSDK.getIntPreference(mCtx,"100_25");
+        n26 = mPowerSDK.getIntPreference(mCtx,"26") + mPowerSDK.getIntPreference(mCtx,"10_26") + mPowerSDK.getIntPreference(mCtx,"100_26");
+        n27 = mPowerSDK.getIntPreference(mCtx,"27") + mPowerSDK.getIntPreference(mCtx,"10_27") + mPowerSDK.getIntPreference(mCtx,"100_27");
+        n28 = mPowerSDK.getIntPreference(mCtx,"28") + mPowerSDK.getIntPreference(mCtx,"10_28") + mPowerSDK.getIntPreference(mCtx,"100_28");
+        n29 = mPowerSDK.getIntPreference(mCtx,"29") + mPowerSDK.getIntPreference(mCtx,"10_29") + mPowerSDK.getIntPreference(mCtx,"100_29");
+        n30 = mPowerSDK.getIntPreference(mCtx,"30") + mPowerSDK.getIntPreference(mCtx,"10_30") + mPowerSDK.getIntPreference(mCtx,"100_30");
+
+        n31 = mPowerSDK.getIntPreference(mCtx,"31") + mPowerSDK.getIntPreference(mCtx,"10_31") + mPowerSDK.getIntPreference(mCtx,"100_31");
+        n32 = mPowerSDK.getIntPreference(mCtx,"32") + mPowerSDK.getIntPreference(mCtx,"10_32") + mPowerSDK.getIntPreference(mCtx,"100_32");
+        n33 = mPowerSDK.getIntPreference(mCtx,"33") + mPowerSDK.getIntPreference(mCtx,"10_33") + mPowerSDK.getIntPreference(mCtx,"100_33");
+        n34 = mPowerSDK.getIntPreference(mCtx,"34") + mPowerSDK.getIntPreference(mCtx,"10_34") + mPowerSDK.getIntPreference(mCtx,"100_34");
+        n35 = mPowerSDK.getIntPreference(mCtx,"35") + mPowerSDK.getIntPreference(mCtx,"10_35") + mPowerSDK.getIntPreference(mCtx,"100_35");
+        n36 = mPowerSDK.getIntPreference(mCtx,"36") + mPowerSDK.getIntPreference(mCtx,"10_36") + mPowerSDK.getIntPreference(mCtx,"100_36");
+        n37 = mPowerSDK.getIntPreference(mCtx,"37") + mPowerSDK.getIntPreference(mCtx,"10_37") + mPowerSDK.getIntPreference(mCtx,"100_37");
+        n38 = mPowerSDK.getIntPreference(mCtx,"38") + mPowerSDK.getIntPreference(mCtx,"10_38") + mPowerSDK.getIntPreference(mCtx,"100_38");
+        n39 = mPowerSDK.getIntPreference(mCtx,"39") + mPowerSDK.getIntPreference(mCtx,"10_39") + mPowerSDK.getIntPreference(mCtx,"100_39");
+        n40 = mPowerSDK.getIntPreference(mCtx,"40") + mPowerSDK.getIntPreference(mCtx,"10_40") + mPowerSDK.getIntPreference(mCtx,"100_40");
+
+        n41 = mPowerSDK.getIntPreference(mCtx,"41") + mPowerSDK.getIntPreference(mCtx,"10_41") + mPowerSDK.getIntPreference(mCtx,"100_41");
+        n42 = mPowerSDK.getIntPreference(mCtx,"42") + mPowerSDK.getIntPreference(mCtx,"10_42") + mPowerSDK.getIntPreference(mCtx,"100_42");
+        n43 = mPowerSDK.getIntPreference(mCtx,"43") + mPowerSDK.getIntPreference(mCtx,"10_43") + mPowerSDK.getIntPreference(mCtx,"100_43");
+        n44 = mPowerSDK.getIntPreference(mCtx,"44") + mPowerSDK.getIntPreference(mCtx,"10_44") + mPowerSDK.getIntPreference(mCtx,"100_44");
+        n45 = mPowerSDK.getIntPreference(mCtx,"45") + mPowerSDK.getIntPreference(mCtx,"10_45") + mPowerSDK.getIntPreference(mCtx,"100_45");
 
         if(n <= 45)
         {
