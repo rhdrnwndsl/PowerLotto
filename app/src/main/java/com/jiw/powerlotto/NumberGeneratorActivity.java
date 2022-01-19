e53d3e9594b6de81b07aeaa8110cc27d08fb5435
rhdrnwndsl <tlswlsdn2457@gmail.com>
2022-01-20 01:26:48 +0900

    보너스번호는 2등일경우에만 체크하기 때문에 번호추출 시 보너스번호를 포함해서 처리하면 오히려 효율이 덜어지는 것 같아서 보너스포함한 번호는 1회. 그이외는 보너스미포함으로 번호를 추출한다

--- app/src/main/java/com/jiw/powerlotto/NumberGeneratorActivity.java
+++ app/src/main/java/com/jiw/powerlotto/NumberGeneratorActivity.java
@@ -201,7 +201,7 @@ public class NumberGeneratorActivity extends AppCompatActivity {
         int finalMax = max;
         adapter = new ListNumberGenerateAdapter();
         FindCombine();
-        while(adapter.m_list_number_reader.size() < 2 )
+        while(adapter.m_list_number_reader.size() < 1 )
         {
             NumberSet(finalMax);
             try
@@ -213,73 +213,13 @@ public class NumberGeneratorActivity extends AppCompatActivity {
             }
         }
 
-        Top15Init();
-
         NotBonusInit();
+
+        Top20Init();
 
         listview.setAdapter(adapter);
         ButtonEffect();
 
-    }
-
-    private void Top15Init()
-    {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("1", mPowerSDK.getIntPreference(mCtx,"1"));map.put("11", mPowerSDK.getIntPreference(mCtx,"11"));map.put("21", mPowerSDK.getIntPreference(mCtx,"21"));
-        map.put("2", mPowerSDK.getIntPreference(mCtx,"2"));map.put("12", mPowerSDK.getIntPreference(mCtx,"12"));map.put("22", mPowerSDK.getIntPreference(mCtx,"22"));
-        map.put("3", mPowerSDK.getIntPreference(mCtx,"3"));map.put("13", mPowerSDK.getIntPreference(mCtx,"13"));map.put("23", mPowerSDK.getIntPreference(mCtx,"23"));
-        map.put("4", mPowerSDK.getIntPreference(mCtx,"4"));map.put("14", mPowerSDK.getIntPreference(mCtx,"14"));map.put("24", mPowerSDK.getIntPreference(mCtx,"24"));
-        map.put("5", mPowerSDK.getIntPreference(mCtx,"5"));map.put("15", mPowerSDK.getIntPreference(mCtx,"15"));map.put("25", mPowerSDK.getIntPreference(mCtx,"25"));
-        map.put("6", mPowerSDK.getIntPreference(mCtx,"6"));map.put("16", mPowerSDK.getIntPreference(mCtx,"16"));map.put("26", mPowerSDK.getIntPreference(mCtx,"26"));
-        map.put("7", mPowerSDK.getIntPreference(mCtx,"7"));map.put("17", mPowerSDK.getIntPreference(mCtx,"17"));map.put("27", mPowerSDK.getIntPreference(mCtx,"27"));
-        map.put("8", mPowerSDK.getIntPreference(mCtx,"8"));map.put("18", mPowerSDK.getIntPreference(mCtx,"18"));map.put("28", mPowerSDK.getIntPreference(mCtx,"28"));
-        map.put("9", mPowerSDK.getIntPreference(mCtx,"9"));map.put("19", mPowerSDK.getIntPreference(mCtx,"19"));map.put("29", mPowerSDK.getIntPreference(mCtx,"29"));
-        map.put("10", mPowerSDK.getIntPreference(mCtx,"10")); map.put("20", mPowerSDK.getIntPreference(mCtx,"20")); map.put("30", mPowerSDK.getIntPreference(mCtx,"30"));
-
-        map.put("31", mPowerSDK.getIntPreference(mCtx,"31"));map.put("41", mPowerSDK.getIntPreference(mCtx,"41"));
-        map.put("32", mPowerSDK.getIntPreference(mCtx,"32"));map.put("42", mPowerSDK.getIntPreference(mCtx,"42"));
-        map.put("33", mPowerSDK.getIntPreference(mCtx,"33"));map.put("43", mPowerSDK.getIntPreference(mCtx,"43"));
-        map.put("34", mPowerSDK.getIntPreference(mCtx,"34"));map.put("44", mPowerSDK.getIntPreference(mCtx,"44"));
-        map.put("35", mPowerSDK.getIntPreference(mCtx,"35"));map.put("45", mPowerSDK.getIntPreference(mCtx,"45"));
-        map.put("36", mPowerSDK.getIntPreference(mCtx,"36"));
-        map.put("37", mPowerSDK.getIntPreference(mCtx,"37"));
-        map.put("38", mPowerSDK.getIntPreference(mCtx,"38"));
-        map.put("39", mPowerSDK.getIntPreference(mCtx,"39"));
-        map.put("40", mPowerSDK.getIntPreference(mCtx,"40"));
-
-
-        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
-        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        });
-        int top10Count = 0;
-        int _count = 0;
-        for(Map.Entry<String, Integer> entry : entryList){
-            if(_count == 15)
-            {
-                break;
-            }
-            System.out.println("top15key : " + entry.getKey() + ", top15value : " + entry.getValue());
-            top10Count+=entry.getValue();
-            _count++;
-        }
-
-        FindCombine();
-
-        while(adapter.m_list_number_reader.size() < 3 )
-        {
-            NumberSet15(top10Count, entryList);
-            try
-            {
-                Thread.sleep(200);
-            } catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
     }
 
     private void NotBonusInit()
@@ -380,7 +320,7 @@ public class NumberGeneratorActivity extends AppCompatActivity {
 
         int finalMax = max;
         FindCombineNotBonus();
-        while(adapter.m_list_number_reader.size() < 5 )
+        while(adapter.m_list_number_reader.size() < 4 )
         {
             NumberSet(finalMax);
             try
@@ -393,6 +333,66 @@ public class NumberGeneratorActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    private void Top20Init()
+    {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("1", mPowerSDK.getIntPreference(mCtx,"1"));map.put("11", mPowerSDK.getIntPreference(mCtx,"11"));map.put("21", mPowerSDK.getIntPreference(mCtx,"21"));
+        map.put("2", mPowerSDK.getIntPreference(mCtx,"2"));map.put("12", mPowerSDK.getIntPreference(mCtx,"12"));map.put("22", mPowerSDK.getIntPreference(mCtx,"22"));
+        map.put("3", mPowerSDK.getIntPreference(mCtx,"3"));map.put("13", mPowerSDK.getIntPreference(mCtx,"13"));map.put("23", mPowerSDK.getIntPreference(mCtx,"23"));
+        map.put("4", mPowerSDK.getIntPreference(mCtx,"4"));map.put("14", mPowerSDK.getIntPreference(mCtx,"14"));map.put("24", mPowerSDK.getIntPreference(mCtx,"24"));
+        map.put("5", mPowerSDK.getIntPreference(mCtx,"5"));map.put("15", mPowerSDK.getIntPreference(mCtx,"15"));map.put("25", mPowerSDK.getIntPreference(mCtx,"25"));
+        map.put("6", mPowerSDK.getIntPreference(mCtx,"6"));map.put("16", mPowerSDK.getIntPreference(mCtx,"16"));map.put("26", mPowerSDK.getIntPreference(mCtx,"26"));
+        map.put("7", mPowerSDK.getIntPreference(mCtx,"7"));map.put("17", mPowerSDK.getIntPreference(mCtx,"17"));map.put("27", mPowerSDK.getIntPreference(mCtx,"27"));
+        map.put("8", mPowerSDK.getIntPreference(mCtx,"8"));map.put("18", mPowerSDK.getIntPreference(mCtx,"18"));map.put("28", mPowerSDK.getIntPreference(mCtx,"28"));
+        map.put("9", mPowerSDK.getIntPreference(mCtx,"9"));map.put("19", mPowerSDK.getIntPreference(mCtx,"19"));map.put("29", mPowerSDK.getIntPreference(mCtx,"29"));
+        map.put("10", mPowerSDK.getIntPreference(mCtx,"10")); map.put("20", mPowerSDK.getIntPreference(mCtx,"20")); map.put("30", mPowerSDK.getIntPreference(mCtx,"30"));
+
+        map.put("31", mPowerSDK.getIntPreference(mCtx,"31"));map.put("41", mPowerSDK.getIntPreference(mCtx,"41"));
+        map.put("32", mPowerSDK.getIntPreference(mCtx,"32"));map.put("42", mPowerSDK.getIntPreference(mCtx,"42"));
+        map.put("33", mPowerSDK.getIntPreference(mCtx,"33"));map.put("43", mPowerSDK.getIntPreference(mCtx,"43"));
+        map.put("34", mPowerSDK.getIntPreference(mCtx,"34"));map.put("44", mPowerSDK.getIntPreference(mCtx,"44"));
+        map.put("35", mPowerSDK.getIntPreference(mCtx,"35"));map.put("45", mPowerSDK.getIntPreference(mCtx,"45"));
+        map.put("36", mPowerSDK.getIntPreference(mCtx,"36"));
+        map.put("37", mPowerSDK.getIntPreference(mCtx,"37"));
+        map.put("38", mPowerSDK.getIntPreference(mCtx,"38"));
+        map.put("39", mPowerSDK.getIntPreference(mCtx,"39"));
+        map.put("40", mPowerSDK.getIntPreference(mCtx,"40"));
+
+
+        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        int top20Count = 0;
+        int _count = 0;
+        for(Map.Entry<String, Integer> entry : entryList){
+            if(_count == 20)
+            {
+                break;
+            }
+            System.out.println("top20key : " + entry.getKey() + ", top20value : " + entry.getValue());
+            top20Count+=entry.getValue();
+            _count++;
+        }
+
+        FindCombineNotBonus();
+
+        while(adapter.m_list_number_reader.size() < 5 )
+        {
+            NumberSet20(top20Count, entryList);
+            try
+            {
+                Thread.sleep(200);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
 
@@ -517,7 +517,7 @@ public class NumberGeneratorActivity extends AppCompatActivity {
 
     }
 
-    private void NumberSet15(int _max, List<Map.Entry<String, Integer>> entryList)
+    private void NumberSet20(int _max, List<Map.Entry<String, Integer>> entryList)
     {
         Set<Integer> set = new HashSet<>();
         int i = 0;
@@ -542,12 +542,12 @@ public class NumberGeneratorActivity extends AppCompatActivity {
         while (set.size() < 6) {
 
             int max = 0;
-            int _count15 = 0;
+            int _count20 = 0;
             Map<String, Integer> _tmpMap = new HashMap<>();
             for ( String _key:SwitchMap.keySet()) {
                 if(_key.contains("." + String.valueOf(i) + ",") || _key.contains("," + String.valueOf(i) + "."))
                 {
-                    if (_count15 == 15)
+                    if (_count20 == 20)
                     {
                         break;
                     }
@@ -556,7 +556,7 @@ public class NumberGeneratorActivity extends AppCompatActivity {
                     _tmp = _tmp.replace("," + String.valueOf(i),"");
                     _tmp = _tmp.replace("." + String.valueOf(i),"");
                     _tmpMap.put(_tmp, SwitchMap.get(_key));
-                    _count15++;
+                    _count20++;
 
                 }
             }
